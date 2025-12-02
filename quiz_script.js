75e2c5978d0ec9cf88ae623c2038274fe6cde26d
小银狐wolfi <nilsschmalhaus@gmail.com>
2025-12-02 16:51:05 +0100

    Add files via upload

--- quiz_script.js
+++ quiz_script.js
@@ -1,15 +1,16 @@
 const pageName = window.location.pathname.split("/").pop().replace(".html", "");
 const quizKey = "quizScore_" + pageName;
+const colorKey = "quizColored_" + pageName;
 
 const quizForm = document.getElementById("quizForm");
 const quizResult = document.getElementById("quizResult");
 const submitBtn = document.getElementById("submitQuiz");
 
 const savedScore = localStorage.getItem(quizKey);
+const colored = localStorage.getItem(colorKey);
 
 if (savedScore !== null) {
   const score = parseInt(savedScore);
-
   if (score <= 0) {
     quizForm.querySelectorAll("input").forEach((inp) => (inp.disabled = false));
     submitBtn.disabled = false;
@@ -29,11 +30,28 @@ submitBtn.addEventListener("click", function () {
   const answers = ["q1", "q2", "q3"];
 
   answers.forEach((q) => {
-    const answer = quizForm.querySelector(`input[name="${q}"]:checked`);
-    if (answer && answer.value === "1") score++;
+    const correctInput = quizForm.querySelector(
+      `input[name="${q}"][value="1"]`
+    );
+    const userInput = quizForm.querySelector(`input[name="${q}"]:checked`);
+
+    if (userInput && userInput.value === "1") score++;
+
+    if (!colored) {
+      if (userInput) {
+        if (userInput.value === "1") {
+          userInput.parentElement.style.color = "green";
+        } else {
+          userInput.parentElement.style.color = "red";
+
+          correctInput.parentElement.style.color = "green";
+        }
+      }
+    }
   });
 
   localStorage.setItem(quizKey, score);
+  localStorage.setItem(colorKey, "1");
 
   quizResult.innerHTML = `<h3>Du hast ${score} / 3 richtig!</h3>`;
 
