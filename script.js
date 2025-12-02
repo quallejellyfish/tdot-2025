052019c1a8387840043b1054da1c996d9553e046
小银狐wolfi <nilsschmalhaus@gmail.com>
2025-12-02 16:28:47 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const iconMenu = document.querySelector(".icon_menu");
 const menuList = document.querySelector(".menu_list");
+const wrapper = document.querySelector(".wrapper");
 
-iconMenu.addEventListener("click", () => {
+iconMenu.addEventListener("click", (e) => {
+  e.stopPropagation();
   iconMenu.classList.toggle("open");
   menuList.classList.toggle("open");
+});
+
+document.addEventListener("click", (e) => {
+  iconMenu.classList.remove("open");
+  menuList.classList.remove("open");
 });
