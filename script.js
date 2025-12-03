85b7c17b30a035ba849a06afb860921338f503df
小银狐wolfi <nilsschmalhaus@gmail.com>
2025-12-03 16:41:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ document.addEventListener("click", (e) => {
   iconMenu.classList.remove("open");
   menuList.classList.remove("open");
 });
+
+let currentImage = 0;
+const slides = document.querySelectorAll(".slide");
+const total = slides.length;
+
+// Show the first image
+slides[currentImage].classList.add("active");
+
+function showSlide(index) {
+  slides[currentImage].classList.remove("active");
+  currentImage = (index + total) % total;
+  slides[currentImage].classList.add("active");
+}
+
+document.querySelector(".arrow.left").addEventListener("click", () => {
+  showSlide(currentImage - 1);
+});
+
+document.querySelector(".arrow.right").addEventListener("click", () => {
+  showSlide(currentImage + 1);
+});
